8586ac270f269cb837b0984aa30c662cbbd2a004
kwinkunks <matt@agilegeoscience.com>
2014-06-09 12:16:17 -0300

    model page and help

--- main.py
+++ main.py
@@ -1795,12 +1795,12 @@ class ModelBuilder(ModelrPageRequest):
 
         ImageModel(parent=user,
                    user=user.user_id, image=blob_key).put()
-        # TODO LOgging
+        # TODO Logging
 
-class SectionModel(ModelrPageRequest):
+
+class ModelHandler(ModelrPageRequest):
 
     def get(self):
-        
         user = ModelrPageRequest.verify(self)
         if user is None:
             self.redirect('/signup')
@@ -1852,14 +1852,12 @@ class SectionModel(ModelrPageRequest):
         if self.request.get("error"):
             params.update(error="Invalid image file")
         
-        template = env.get_template('section_model.html')
+        template = env.get_template('model.html')
         html = template.render(params)
         self.response.out.write(html)
 
 
 class ImageModelHandler(ModelrPageRequest):
-
-    
 
     def get(self):
 
@@ -2081,8 +2079,6 @@ class AdminHandler(ModelrPageRequest):
         html = template.render(user=user)
         self.response.out.write(html)
         
-
-        
     def post(self):
 
         user = self.verify()
@@ -2120,7 +2116,6 @@ class AdminHandler(ModelrPageRequest):
                 self.response.out.write(html)
 
 
-
 class ServerError(ModelrPageRequest):
 
     def post(self):
@@ -2155,7 +2150,7 @@ app = webapp2.WSGIApplication([('/', MainHandler),
                                ('/manage_group', ManageGroup),
                                ('/upload', Upload),
                                ('/model_builder', ModelBuilder),
-                               ('/section_model', SectionModel),
+                               ('/model', ModelHandler),
                                ('/image_model', ImageModelHandler),
                                ('/earth_model', EarthModelHandler),
                                ('/forgot', ForgotHandler),
