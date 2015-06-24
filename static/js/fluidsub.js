aad8f4e68af8e62ad083328513995372810a9291
ben-bougher <ben.bougher@gmail.com>
2015-06-24 07:17:06 -0700

    #167. changed to Alt

--- static/js/fluidsub.js
+++ static/js/fluidsub.js
@@ -484,7 +484,7 @@ function FluidSub(canvas, core_width, core_height,
 
     function fluidsub_click(d,i){
 
-    if (d3.event.ctrlKey & d3.event.shiftKey){
+    if (d3.event.altKey){
 	// delete sub fluid
 	// always keep one layer
         if (i > 0){
@@ -545,7 +545,7 @@ function FluidSub(canvas, core_width, core_height,
     function rock_click(d, i, j){
 
     // Open the menu
-        if (d3.event.ctrlKey & d3.event.shiftKey) {
+        if (d3.event.altKey) {
 	    delete_interval(d, i);
             return;
         } else {
