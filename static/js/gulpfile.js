e90c661ae3380e945f047bf5c43ae0d81c59cf65
System Administrator <justinkheisler@gmail.com>
2015-09-02 11:49:51 -0300

    adding gulpfile, js build, including files now from bower instead of through cdn, added angular

---
+++ static/js/gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var watch = require('gulp-watch');
+ 
+gulp.task('default', ['jshint', 'concat']);
+
+var js;
+gulp.task('concat', function (cb) {
+    var options = {};
+    watch('controllers/*.js', options, function (e) {
+        // console.log('e:'+JSON.stringify(e));
+        // console.log('\n');
+        console.log(new Date() + ' -- ' + e.history[0].replace(e.base, ''));
+        //js = e.history[0].replace(e.base, '');
+        gulp.src(['controllers/*.js'])
+        .pipe(concat('app.js', {newLine: '\n'}))
+        .pipe(gulp.dest('./'));
+    });
+});
+
+// Lint JavaScript
+gulp.task('jshint', function () {
+  return gulp.src([
+      'controllers/*.js'
+    ])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
