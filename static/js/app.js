862afa74c054076d7aa9637fe77af9aaf672d983
Ben Bougher <ben.bougher@gmail.com>
2015-09-03 10:10:58 -0300

    updating 2d model

--- static/js/app.js
+++ static/js/app.js
@@ -6,31 +6,36 @@ app.config(['$interpolateProvider', function($interpolateProvider) {
   $interpolateProvider.endSymbol(']}');
 }]);
 
-app.controller('2DCtrl', function ($scope) {
-	$scope.popover = {
-		title: "Models",
-		content: "Choose a model framework from the carousel below, or use the buttons to the right to upload an image or create a new model with the model builder. then assign the model's rocks and other parameters in the panel to the right."
-	};
+app.controller('2DCtrl', function ($scope, $http) {
+    $scope.popover = {
+	title: "Models",
+	content: "Choose a model framework from the carousel below, or use the buttons to the right to upload an image or create a new model with the model builder. then assign the model's rocks and other parameters in the panel to the right."
+    };
 
-	//$scope.savedEarthModel = {data:[3,4,5,6,2]};
-	$scope.model1 = {title: "Model 1", data:[
-		{ rock:'rock1', color: "#fef" }, 
-		{ rock: 'rock2', color: "fff" }, 
-		{ rock: 'rock3', color: "ddd" }]};
-	$scope.model2 = {title: "Model 2", data:[
-		{ rock:'rock1', color: "#fef" }]};
+    // check for saved earth models
+    $http.get('/earth_model?all').
+        then(function(response) {
 
-	$scope.rocks = ['rock1', 'rock2', 'rock3', 'rock4', 'rock'];
-
-	$scope.earthModels = [$scope.model1, $scope.model2];
-	console.log($scope.earthModels);
-
-	$scope.test = function(model){
-		//console.log($scope.savedEarthModel.data);
-		//console.log(model);
-		console.log(model);
-		//console.log($scope.testatt);
-		//$scope.savedEarthModel = {data:[3,6,2]};
-	}
-
+            var em = response.data;
+            $scope.earthModels = em;
+        });
+    
+    // populate the rocks
+    $http.get('/rock?all').
+        then(function(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+            var rocks = response.data;
+            $scope.rocks = rocks;
+        });
+             
+    console.log($scope.earthModels);
+    
+    $scope.test = function(model){
+	//console.log($scope.savedEarthModel.data);
+	//console.log(model);
+	console.log(model);
+	//console.log($scope.testatt);
+	//$scope.savedEarthModel = {data:[3,6,2]};
+    };
 });
